f05f0052ef4e1158b1fd5c9489b3b516b364395b
BayuDC <dev.bayudc@gmail.com>
2024-10-03 00:10:17 +0700

    feat: simple toast alert

---
+++ components/Toast.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+const props = defineProps<{
+  message: string;
+  color: 'rose' | 'green' | 'blue' | 'yellow';
+  icon: string;
+}>();
+const emit = defineEmits<{
+  (e: 'dispose'): void;
+}>();
+
+onMounted(() => {
+  setTimeout(() => {
+    emit('dispose');
+  }, 3000);
+});
+</script>
+
+<template>
+  <div class="absolute w-full max-w-md bottom-0 left-[50%] px-5 md:px-10 translate-x-[-50%] translate-y-[-40%]">
+    <UAlert :icon="icon" :color="color" variant="solid" :title="message" />
+  </div>
+</template>
+
+<style scoped>
+.toast-enter-active,
+.toast-leave-active {
+  @apply transition duration-300;
+}
+.toast-enter-from,
+.toast-leave-to {
+  @apply translate-x-[-50%] translate-y-[100%];
+}
+</style>
--- pages/login.vue
+++ pages/login.vue
@@ -16,6 +16,7 @@ const { data, execute, status, clear } = useMyFetch<{ token: string }>('/auth/lo
   },
   body: state,
   watch: false,
+  immediate: false,
 });
 
 async function onSubmit() {
@@ -26,46 +27,68 @@ async function onSubmit() {
   }
 
   token.value = data.value.token;
+  setTimeout(() => {
+    clear();
+    navigateTo('/');
+  }, 1000);
 }
 </script>
 
 <template>
-  <div class="max-w-md mx-auto h-screen flex items-center">
-    <Main class="w-full">
-      <Section title="Electra Store" center-head>
-        <div>
-          <UCard>
-            <UForm :state="state" class="space-y-4" @submit="onSubmit">
-              <UFormGroup label="Email" name="email">
-                <UInput v-model="state.email" />
-              </UFormGroup>
+  <div class="h-screen relative">
+    <div class="max-w-md mx-auto h-full flex items-center">
+      <Main class="w-full">
+        <Section title="Electra Store" center-head>
+          <div>
+            <UCard>
+              <UForm :state="state" class="space-y-4" @submit="onSubmit">
+                <UFormGroup label="Email" name="email">
+                  <UInput v-model="state.email" />
+                </UFormGroup>
 
-              <UFormGroup label="Password" name="password">
-                <UInput v-model="state.password" type="password" />
-              </UFormGroup>
+                <UFormGroup label="Password" name="password">
+                  <UInput v-model="state.password" type="password" />
+                </UFormGroup>
 
-              <div class="flex">
-                <UButton type="submit" class="ml-auto" :loading="status == 'pending'"> Login </UButton>
-              </div>
-            </UForm>
-          </UCard>
-          <div class="mt-6 flex gap-4 flex-col">
-            <UButton color="gray" variant="solid" class="w-full py-3 px-4 sm:px-6">
-              <div class="flex items-center w-full gap-4 justify-between">
-                <span> Continue with Google </span>
-                <img src="~/assets/google.svg" alt="" />
-              </div>
-            </UButton>
-            <UButton color="gray" variant="solid" class="w-full py-3 px-4 sm:px-6">
-              <div class="flex items-center w-full gap-4 justify-between">
-                <span> Continue with Apple </span>
-                <img src="~/assets/apple.svg" alt="" />
-              </div>
-            </UButton>
+                <div class="flex">
+                  <UButton type="submit" class="ml-auto" :loading="status == 'pending'"> Login </UButton>
+                </div>
+              </UForm>
+            </UCard>
+            <div class="mt-6 flex gap-4 flex-col">
+              <UButton color="gray" variant="solid" class="w-full py-3 px-4 sm:px-6">
+                <div class="flex items-center w-full gap-4 justify-between">
+                  <span> Continue with Google </span>
+                  <img src="~/assets/google.svg" alt="" />
+                </div>
+              </UButton>
+              <UButton color="gray" variant="solid" class="w-full py-3 px-4 sm:px-6">
+                <div class="flex items-center w-full gap-4 justify-between">
+                  <span> Continue with Apple </span>
+                  <img src="~/assets/apple.svg" alt="" />
+                </div>
+              </UButton>
+            </div>
           </div>
-        </div>
-      </Section>
-    </Main>
+        </Section>
+      </Main>
+      <Transition name="toast" mode="out-in">
+        <Toast
+          v-if="status == 'error'"
+          icon="i-heroicons-x-circle-16-solid"
+          message="Log in failed!"
+          color="rose"
+          @dispose="clear"
+        />
+        <Toast
+          v-else-if="status == 'success'"
+          icon="i-heroicons-check-circle-20-solid"
+          message="Log in success!"
+          color="green"
+          @dispose="clear"
+        />
+      </Transition>
+    </div>
   </div>
 </template>
 
