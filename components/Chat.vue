0b1eb6436e0a08e62ac583ed700bda6a06bf2196
BayuDC <dev.bayudc@gmail.com>
2024-10-24 01:34:06 +0700

    feat: sending messages

--- components/Chat.vue
+++ components/Chat.vue
@@ -1,4 +1,29 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const user = useAuth();
+const message = ref('');
+
+const { data: messages } = await useMyFetch<
+  {
+    content: string;
+    sender_id: number;
+  }[]
+>('/messages/1');
+
+async function sendMessage() {
+  const {} = await useMyFetch('/messages/1', {
+    method: 'POST',
+    body: JSON.stringify({
+      content: message.value,
+    }),
+  });
+
+  messages.value!.push({
+    content: message.value,
+    sender_id: user.value!.id,
+  });
+  message.value = '';
+}
+</script>
 
 <template>
   <UCard
@@ -9,12 +34,11 @@
     }"
   >
     <div class="flex flex-col gap-4">
-      <ChatMessage type="you">Hello Buyer</ChatMessage>
-      <ChatMessage type="me">Hello Seller</ChatMessage>
+      <ChatMessage v-for="m in messages" :type="m.sender_id == user?.id ? 'me' : 'you'">{{ m.content }}</ChatMessage>
     </div>
     <UDivider class="my-6" />
     <div>
-      <UInput placeholder="Type here...." size="xl" />
+      <UInput placeholder="Type here...." size="xl" @keyup.enter="sendMessage" v-model="message" />
     </div>
   </UCard>
 </template>
